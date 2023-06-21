c6ed089d275f10e187b74ee2d2ec364f4a15007a
viqueen <hasnae@viqueen.org>
2023-06-21 12:34:52 +1000

    prepare apache-2.0 license

--- src/main/java/org/viqueen/ide/links/JiraLinksReferenceProvider.java
+++ src/main/java/org/viqueen/ide/links/JiraLinksReferenceProvider.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2023 Hasnae Rehioui
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.viqueen.ide.links;
 
 import com.intellij.openapi.paths.WebReference;
@@ -31,7 +46,7 @@ public class JiraLinksReferenceProvider extends PsiReferenceProvider {
                 int start = matcher.start("jiraIssue");
                 int end = matcher.end("jiraIssue");
                 TextRange range = new TextRange(start, end);
-                webReferences.add(new WebReference(element, range, format("https://project-links-navigator.web.app/#/jira/%s",jiraIssue)));
+                webReferences.add(new WebReference(element, range, format("https://project-links-navigator.web.app/#/jira/%s", jiraIssue)));
             }
             return webReferences.toArray(new PsiReference[0]);
         }
